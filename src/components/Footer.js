c94eb8bfec779dc15b435dad914a6aa4d83817bd
mohan <mohanp.18cse@kongu.edu>
2021-12-03 22:36:17 +0530

    '.'

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            <h1>This from Footer Commponent Bro</h1>
+            
+        </div>
+    )
+}
+
+export default Footer
+
+ 
